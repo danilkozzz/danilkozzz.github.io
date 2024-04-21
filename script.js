83aadc47b74e07cd48a73b00a118d7143d7268bc
Daniil <daniil@notdiy.studio>
2024-04-21 22:51:25 +0200

    hello

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ document.addEventListener("DOMContentLoaded", function() {
     console.log("hello!");
     console.log("hello2!");
     console.log("hello3!");
+    console.log("hello4!");
     
     function createScrollTrigger(triggerElement, timeline) {
         ScrollTrigger.create({
